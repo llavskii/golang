a4209cadd458c2742d6a507ee753b1530460bbca
llavskii <kieaki@gmail.com>
2022-06-14 13:24:23 +0300

    refactor after linters check

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -13,16 +13,14 @@ func Unpack(source string) (string, error) {
 	if notValidString != nil {
 		return source, notValidString
 	}
-
 	var result = ""
 	runes := []rune(source)
-
 	for i, char := range runes {
 		if unicode.IsDigit(char) {
 			continue
 		}
 		var repeat int
-		if i+1 == len(runes) || !unicode.IsDigit(runes[i+1]) { //Is it last symbol or next symbol is not digit?
+		if i+1 == len(runes) || !unicode.IsDigit(runes[i+1]) { // Is it last symbol or next symbol is not digit?
 			repeat = 1
 		} else {
 			repeat = int(runes[i+1] - '0')
@@ -34,19 +32,16 @@ func Unpack(source string) (string, error) {
 }
 
 func checkPackedString(source string) error {
-	if len(source) == 0 { //Is empty string?
+	if len(source) == 0 { // Is empty string?
 		return nil
 	}
-
-	for i, char := range source { //Check that first char is not number
+	for i, char := range source { // Check that first char is not number
 		if i == 0 && unicode.IsDigit(char) {
 			return ErrInvalidString
 		}
 	}
-
 	var isPreviousNumber bool
-	//Search sequence of numbers
-	for _, char := range source {
+	for _, char := range source { // Search sequence of numbers
 		if unicode.IsDigit(char) {
 			if isPreviousNumber {
 				return ErrInvalidString
