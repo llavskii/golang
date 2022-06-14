55422d4c97b0e1168e80435ba8ccb8ff50dad7bc
llavskii <kieaki@gmail.com>
2022-06-14 13:16:24 +0300

    implement Unpack function

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,59 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(source string) (string, error) {
+	notValidString := checkPackedString(source)
+	if notValidString != nil {
+		return source, notValidString
+	}
+
+	var result = ""
+	runes := []rune(source)
+
+	for i, char := range runes {
+		if unicode.IsDigit(char) {
+			continue
+		}
+		var repeat int
+		if i+1 == len(runes) || !unicode.IsDigit(runes[i+1]) { //Is it last symbol or next symbol is not digit?
+			repeat = 1
+		} else {
+			repeat = int(runes[i+1] - '0')
+		}
+		result += strings.Repeat(string(char), repeat)
+	}
+
+	return result, nil
+}
+
+func checkPackedString(source string) error {
+	if len(source) == 0 { //Is empty string?
+		return nil
+	}
+
+	for i, char := range source { //Check that first char is not number
+		if i == 0 && unicode.IsDigit(char) {
+			return ErrInvalidString
+		}
+	}
+
+	var isPreviousNumber bool
+	//Search sequence of numbers
+	for _, char := range source {
+		if unicode.IsDigit(char) {
+			if isPreviousNumber {
+				return ErrInvalidString
+			}
+			isPreviousNumber = true
+		} else {
+			isPreviousNumber = false
+		}
+	}
+	return nil
 }
