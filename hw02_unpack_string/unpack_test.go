4899c6124aea142bd8b174b610323308921680ff
stanislav <s.lyboslavskiy@propellerads.net>
2022-06-18 14:41:07 +0300

    add positive test

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -15,6 +15,7 @@ func TestUnpack(t *testing.T) {
 		{input: "a4bc2d5e", expected: "aaaabccddddde"},
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
+		{input: "f6", expected: "ffffff"},
 		{input: "aaa0b", expected: "aab"},
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
