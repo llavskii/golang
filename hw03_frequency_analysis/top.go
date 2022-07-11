3a150cfe964e026c2d46b40fbf33c3484344e7db
llavskii <kieaki@gmail.com>
2022-07-11 23:10:02 +0300

    use '`' symbol

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -5,8 +5,7 @@ import (
 	"sort"
 )
 
-var splitRegex = regexp.MustCompile("\\s+") // regexp for split source string
-
+var splitRegex = regexp.MustCompile(`\s+`) // regexp for split source string
 func Top10(source string) []string {
 
 	if len(source) == 0 {
