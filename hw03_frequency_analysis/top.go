b495e0dee1da5a0110d3d76cd3f623a566f0ee80
llavskii <kieaki@gmail.com>
2022-07-11 22:38:34 +0300

    add space after '//'

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -5,27 +5,27 @@ import (
 	"sort"
 )
 
-var splitRegex = regexp.MustCompile("\\s+") //regexp for split source string
+var splitRegex = regexp.MustCompile("\\s+") // regexp for split source string
 
 func Top10(source string) []string {
 
 	if len(source) == 0 {
 		return nil
 	}
-	splitted := splitRegex.Split(source, -1) //split source
-	countDict := map[string]int{}            //map for count of words
+	splitted := splitRegex.Split(source, -1) // split source
+	countDict := map[string]int{}            // map for count of words
 	var max = 0                              // variable for get max count
 	for _, s := range splitted {
 		count := countDict[s] + 1
-		countDict[s] = count //counting this word
+		countDict[s] = count // counting this word
 		if count > max {
 			max = count
 		}
 	}
-	wordsGroupedByCount := map[int][]string{} //map for grouped words by count and sorting them after
+	wordsGroupedByCount := map[int][]string{} // map for grouped words by count and sorting them after
 	for word, counter := range countDict {
 		value, ok := wordsGroupedByCount[counter]
-		if !ok { //if map dos't contain this words counter
+		if !ok { // if map dos't contain this words counter
 			wordsGroupedByCount[counter] = append([]string{}, word)
 		} else { //else append word to slice
 			wordsGroupedByCount[counter] = append(value, word)
